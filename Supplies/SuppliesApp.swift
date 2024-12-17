4aba30e8aadd243a29d16855d57e2bda2fabd94c
polscm32 aka Marvout <marvin-polscheit@t-online.de>
2024-12-17 18:59:49 +0100

    Create App

---
+++ Supplies/SuppliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuppliesApp.swift
+//  Supplies
+//
+//  Created by Marvin Polscheit on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SuppliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
